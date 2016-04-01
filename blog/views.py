d70f19c4737c877e732191d73568cb36daba3102
DoriRunyon <dorileerunyon@gmail.com>
2016-04-01 15:34:59 -0700

    Created post list template.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+    #post_list takes a request and returns a function 'render'
+    #that renders our template blog/post_list.html
+    
